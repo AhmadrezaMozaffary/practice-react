658510fbc813ee37d11fe76e6f16a1de71691dff
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-06-27 20:17:18 +0430

    Add some logic to the form -- 1) Create two states for fetching input values via useState hook -- 2) Listening to events such as onChange or onSubmit -- 3) Start form validations

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../UI/Button";
 import Card from "../UI/Card";
@@ -6,16 +6,42 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length) return;
+    if (+enteredAge < 1) return;
+
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input
+          type="text"
+          id="username"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        />
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" />
+        <input
+          type="number"
+          id="age"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
         <Button type="submit">AddUser</Button>
       </form>
     </Card>
